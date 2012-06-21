c369e98916eac7936dedf1096812b2026c3433c7
Paul Cottle <dashamstyr@gmail.com>
2012-06-21 16:35:34 -0700

    Fourth Commit

--- LNC_plot_v1.py
+++ LNC_plot_v1.py
@@ -1,10 +1,16 @@
-import matplotlib.pyplot as plt
-import pandas as pan
+def plot_test(data):
+    import matplotlib.pyplot as plt
+    import pandas as pan
 
-testdat = pan.load('testdat.pickle')
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    im = ax1.imshow(data, vmin=0, vmax=1)
+    fig.colorbar(im)
+    forceAspect(ax1,aspect = 1.0)
+    
+    plt.show()
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.pcolor(testdat)
-
-plt.show()
+def forceAspect(ax,aspect=1):
+    im = ax.get_images()
+    extent =  im[0].get_extent()
+    ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
