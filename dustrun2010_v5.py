db6f9f2d768dcb3d262ccc4dd6a0e4897dd443ae
dashamstyr <dashamstyr@gmail.com>
2012-06-25 23:17:21 -0700

    Fifth Commit

--- dustrun2010_v5.py
+++ dustrun2010_v5.py
@@ -7,14 +7,14 @@ Whistler = ('WHI',50.128,-122.95) #done
 Egbert = ('EGB',44.232,-79.781) #done
 Yellowknife = ('YKN',62.451,-114.376) #done
 Fort_McMurray = ('FMM',56.752,-111.476) #done
-Kelowna = ('KEL',49.941,-119.400) #April
-Saturna_Island = ('SAT',48.783,-123.133) #April
+Kelowna = ('KEL',49.941,-119.400) #done
+Saturna_Island = ('SAT',48.783,-123.133) #done
 Chapais = ('CHA',49.822,-74.975) #April
-Univ_Leth = ('UOL',49.682,-112.869)
-Bratts_Lake = ('BRA',50.279,-104.7)
+Univ_Leth = ('UOL',49.682,-112.869) #March
+Bratts_Lake = ('BRA',50.279,-104.7) #March
 Sioux_Falls = ('SIO',43.736,96.626)
 Trinidad_head = ('TRI',41.054,-124.151)
-UMBC = ('UMB',39.255,-76.709) #March
+UMBC = ('UMB',39.255,-76.709) #done
 GSFC = ('GSF',38.9925,-76.839833)
 Mauna_Loa = ('MAU',19.539,-155.578003) #Done
 Bonanza_Creek = ('BON',64.742805,-148.316269) #Done
@@ -24,7 +24,7 @@ Dalanzadgad = ('DAL',43.577222,104.419167) #Done
 SACOL = ('SAC',35.946,104.137) #April
 Xiang_he = ('XHE',39.754,116.962) #April
 
-stations = [UMBC]
+stations = [Univ_Leth, Bratts_Lake]
 
 #set heights
 heights = range(1000,10200,200)
